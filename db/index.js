f880cda46305c0b91b25b33b34df3ea762fee31a
Jordan Linton <jordaneddielinton93@hotmail.co.uk>
2021-11-03 13:39:53 +0000

    test

--- db/index.js
+++ db/index.js
@@ -1,13 +1,14 @@
-require('dotenv').config()
+// require('dotenv').config()
 let {Pool}=require("pg")
 
 
 const pool= new Pool({
-  host: process.env.DB_HOST,
-  database:process.env.DB_DATABASE,
-  user:process.env.DB_USER,
-  port: process.env.DB_PORT,
-  password:process.env.DB_PASSWORD,
+  // host: process.env.DB_HOST,
+  // database:process.env.DB_DATABASE,
+  // user:process.env.DB_USER,
+  // port: process.env.DB_PORT,
+  // password:process.env.DB_PASSWORD,
+  connectionString:process.env.DATABASE_URL,
   ssl: {
     rejectUnauthorized: false
   }
